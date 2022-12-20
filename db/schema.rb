8732ba3e6cf5ff859ad39b4f9e776ff2b738965e
marjunstudio <uebujie@gmail.com>
2022-12-20 21:01:19 +0900

    Add: Category, Musicモデルを作成し、CSVでデータを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_18_023454) do
+ActiveRecord::Schema.define(version: 2022_12_18_153931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
