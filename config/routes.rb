674d722b8b6391594138cee23924f04eda7789cb
marjunstudio <uebujie@gmail.com>
2022-12-23 13:51:00 +0900

    Add: youtube検索用のコントローラを作成し、トップ画面のボタンにリンクを埋め込む

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   
   resources :users, only: %i[index new create]
   resources :top_pages, only: %i[top index]
+  resources :youtube_search, only: %i[index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
