94588da8ff8956a0d8d6e4dee0ee23a54fb91372
marjunstudio <uebujie@gmail.com>
2022-12-28 21:20:00 +0900

    Add: updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[index edit update]
   skip_before_action :require_login, only: [:index, :new, :create]
 
   def index
-    @user = current_user
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,8 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :crypted_password)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
