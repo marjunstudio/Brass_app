707082d885bce024168b5207ed7af760a87d6a6d
marjunstudio <uebujie@gmail.com>
2022-12-28 21:18:37 +0900

    Fix: 不要なアクションの削除

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,5 @@
 class TopPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top, :index]
+  skip_before_action :require_login, only: %i[index]
 
   def index
     @categories = Category.all
