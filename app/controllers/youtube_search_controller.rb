62ebeaa7794e95b177ef77d023720e723a381436
marjunstudio <uebujie@gmail.com>
2022-12-29 18:46:55 +0900

    Add: キーワードでyoutubeを検索できるように実装

--- app/controllers/youtube_search_controller.rb
+++ app/controllers/youtube_search_controller.rb
@@ -1,7 +1,13 @@
 class YoutubeSearchController < ApplicationController
   GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
+  
+  def index
+    @youtube_data = find_videos(params[:keyword])
+  end
 
   def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    require 'google/apis/youtube_v3'
+
     service = Google::Apis::YoutubeV3::YouTubeService.new
     service.key = GOOGLE_API_KEY
 
@@ -9,16 +15,12 @@ class YoutubeSearchController < ApplicationController
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 3,
+      max_results: 10,
       order: :date,
       page_token: next_page_token,
       published_after: after.iso8601,
       published_before: before.iso8601
     }
     service.list_searches(:snippet, opt)
-  end
-
-  def index
-    @youtube_data = find_videos('瀬戸弘司')
   end
 end
