c600429721d358cc0bf59df1e7b6bb42aff31659
marjunstudio <uebujie@gmail.com>
2023-01-05 16:21:29 +0900

    Add:youtube検索した時の画面表示を修正

--- app/controllers/search_result_controller.rb
+++ app/controllers/search_result_controller.rb
@@ -2,5 +2,6 @@ class SearchResultController < ApplicationController
   def index
     @q = Music.ransack(params[:q])
     @musics = @q.result
+    @category = Category.where(category_id: @q)
   end
 end
--- app/controllers/youtube_search_controller.rb
+++ app/controllers/youtube_search_controller.rb
@@ -1,5 +1,4 @@
 class YoutubeSearchController < ApplicationController
-  GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
   require 'google/apis/youtube_v3'
   
   def index
@@ -8,16 +7,37 @@ class YoutubeSearchController < ApplicationController
 
   def find_videos(keyword)
     service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = GOOGLE_API_KEY
+    service.key = Rails.application.credentials.google[:api_key]
+    @youtube_data = []
 
+    #動画情報を格納
     next_page_token = nil
-    opt = {
+    video_info = service.list_searches(
+      :snippet,
       q: keyword,
       type: 'video',
       max_results: 10,
       order: :viewCount,
-      page_token: next_page_token,
-    }
-    service.list_searches(:snippet, opt)
+      page_token: next_page_token
+    )
+
+    video_info.items.each_with_index do |item, index|
+      video_id = video_info.items[index].id.video_id
+      video_results = service.list_videos(
+        :statistics,
+        id: video_id,
+        max_results: 1
+      )
+      #動画ID,タイトル,概要, 再生回数を配列で取得
+      view_count = video_results.items[0].statistics.view_count
+      snippet = item.snippet
+
+      @youtube_data << { 
+        video_id: video_id, 
+        title: snippet.title, 
+        description: snippet.description, 
+        published_at: snippet.published_at,
+        view_count: view_count }
+    end
   end
 end
