0fcba1bb83b0256346294d3025cd772a39ea79c0
marjunstudio <uebujie@gmail.com>
2023-01-06 14:17:15 +0900

    Fix:アクション時のメッセージデザインを修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,15 +7,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to(root_path, notice: 'ログイン成功')
+      redirect_back_or_to root_path, success: 'ログインしました'
     else
-      flash.now[:alert] = 'ログイン失敗'
+      flash.now[:warning] = 'ログインに失敗しました'
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to login_path
+    redirect_to login_path, success: 'ログアウトしました'
   end
 end
