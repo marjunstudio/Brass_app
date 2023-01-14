a6014553a0904609f3c412241d109575dea2d471
marjunstudio <uebujie@gmail.com>
2023-01-14 10:22:22 +0900

    Add:TailwindをPostCSS形式で使用

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  content: [
+	mode: 'jit',
+    purge: [
     './app/views/**/*.html.erb',
-    './app/helpers/*.rb',
-    './app/assets/stylesheets/*.scss',
-    './app/javascript/*.js'
-  ]
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
