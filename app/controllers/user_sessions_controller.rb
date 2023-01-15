d7d7fb23a5eeb5ac84f816cd095fb42d0aceed5c
marjunstudio <uebujie@gmail.com>
2023-01-15 21:27:01 +0900

    Fix:フラッシュメッセージをlocalesファイルにまとめた

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,15 +7,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: 'ログインしました'
+      redirect_back_or_to root_path, success: t('.success')
     else
-      flash.now[:warning] = 'ログインに失敗しました'
+      flash.now[:warning] = t('.warning')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to login_path, success: 'ログアウトしました'
+    redirect_to login_path, success: t('.success')
   end
 end
