6b8506d061782752d95edd3e177b65a3896363c4
marjunstudio <uebujie@gmail.com>
2023-02-03 01:03:00 +0900

    Fix: 以前のマイグレーションファイルを修正

--- db/migrate/20221218153931_create_musics.rb
+++ db/migrate/20221218153931_create_musics.rb
@@ -3,10 +3,8 @@ class CreateMusics < ActiveRecord::Migration[6.1]
     create_table :musics do |t|
       t.string :title
       t.string :description
-      # t.references :user, null: false, foreign_key: true
-      # t.references :music, null: false, foreign_key: true
 
-        t.timestamps
+      t.timestamps
     end
   end
 end
