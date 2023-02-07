2e2c4513b4f7dc5a37ef07784a5f6d767c904854
marjunstudio <uebujie@gmail.com>
2023-02-07 21:37:59 +0900

    Add:楽曲名とカテゴリで検索できるように変更

--- app/controllers/search_result_controller.rb
+++ app/controllers/search_result_controller.rb
@@ -1,6 +1,6 @@
 class SearchResultController < ApplicationController
   skip_before_action :require_login
-  before_action :set_music
+  before_action :set_music, only: %i[category composer]
   before_action :set_category, only: %i[category]
   before_action :set_composer, only: %i[composer]
   
@@ -14,5 +14,10 @@ class SearchResultController < ApplicationController
   def composer
     @q = params[:q].values
     @musics = Music.where(composer_id: @q)
+  end
+
+  def word_search
+    @music = Music.ransack(params[:q])
+    @musics = @music.result
   end
 end
