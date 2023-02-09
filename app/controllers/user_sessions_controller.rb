9215b74837714d55458bf9a5e28e5e2617b17bbd
marjunstudio <uebujie@gmail.com>
2023-02-09 22:29:02 +0900

    Fix:ユーザーログインした時の挙動を修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path, success: t('.success')
+      redirect_to root_path, success: t('.success')
     else
       flash.now[:warning] = t('.warning')
       render :new
