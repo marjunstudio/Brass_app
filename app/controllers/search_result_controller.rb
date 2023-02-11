7fbbf7868c4ae5e1118578749088c4731e5390c6
marjunstudio <uebujie@gmail.com>
2023-02-11 22:51:07 +0900

    Change:同時検索できるよう変更

--- app/controllers/search_result_controller.rb
+++ app/controllers/search_result_controller.rb
@@ -1,20 +1,7 @@
 class SearchResultController < ApplicationController
   skip_before_action :require_login
-  before_action :set_music, only: %i[category composer]
-  before_action :set_category, only: %i[category]
-  before_action :set_composer, only: %i[composer]
   
   def index;end
-
-  def category
-    @q = params[:q].values
-    @musics = MusicCategory.where(category_id: @q)
-  end
-
-  def composer
-    @q = params[:q].values
-    @musics = Music.where(composer_id: @q)
-  end
 
   def word_search
     @music = Music.ransack(params[:q])
