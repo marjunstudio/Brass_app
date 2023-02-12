fb40f8e305f5a9cb4fc397a636f8255b5c2aa170
marjunstudio <uebujie@gmail.com>
2023-02-12 13:27:19 +0900

    Change:検索アクションを一つに統一

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,12 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   
+  resources :top_pages, only: %i[index]
   resources :users, only: %i[new create]
-  resource :profile, only: %i[show edit update]
-  resources :top_pages, only: %i[index] do
-    collection do
-      get 'search'
-    end
-  end
-  resources :youtube_search, only: %i[top index]
-  resources :search_result, only: %i[index] do
-    collection do
-      get 'category'
-      get 'composer'
-      get 'word_search'
-    end
-  end
   resources :likes, only: %i[index create destroy]
+
+  resource :profile, only: %i[show edit update]
+
+  resources :youtube_search, only: %i[top index]
+  resources :search_result, only: %i[index]
 end
