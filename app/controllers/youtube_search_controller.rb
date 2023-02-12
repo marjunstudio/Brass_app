57cc0b3294a4d6eaa2d07fe0387c0933eb46185c
marjunstudio <uebujie@gmail.com>
2023-02-12 11:56:27 +0900

    Change:不要なアクションを削除

--- app/controllers/youtube_search_controller.rb
+++ app/controllers/youtube_search_controller.rb
@@ -1,5 +1,5 @@
 class YoutubeSearchController < ApplicationController
-  skip_before_action :require_login, only: %i[index ind_videos]
+  skip_before_action :require_login, only: %i[index]
   require 'google/apis/youtube_v3'
   
   def index
