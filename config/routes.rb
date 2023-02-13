0a80d45533e37f27d59e233f2e69aa73e1e0ad17
marjunstudio <uebujie@gmail.com>
2023-02-13 23:07:21 +0900

    Change:top_pagesコントローラの名称を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'top_pages#index'
+  root 'static_pages#index'
 
+  get 'guide', to: 'static_pages#guide'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   
-  resources :top_pages, only: %i[index]
+  resources :static_pages, only: %i[index]
+
   resources :users, only: %i[new create]
   resources :likes, only: %i[index create destroy]
 
