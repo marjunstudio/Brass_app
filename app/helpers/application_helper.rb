b238995f9391489623e646a6d3d98d98f4d276bf
marjunstudio <uebujie@gmail.com>
2023-02-15 07:10:43 +0900

    Add:OGP画像を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Animatone',
-      title: '吹奏楽曲検索&視聴サービス',
+      title: 'Animatone',
       reverse: true,
       charset: 'utf-8',
       description: 'Animatoneでは吹奏楽曲をタイトル名、カテゴリ、作曲者から検索し、Youtube動画を一覧でみることができます。',
@@ -15,15 +15,15 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        # image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
+        image: image_url('/images/ogp.png'), # 配置するパスやファイル名によって変更すること
         local: 'ja-JP'
       },
       # Twitter用の設定を個別で設定する
-      # twitter: {
-      #   card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
-      #   site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-      #   image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
-      # }
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+        image: image_url('/images/ogp.png') # 配置するパスやファイル名によって変更すること
+      }
     }
   end
 end
