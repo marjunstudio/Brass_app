707976031e5aee2de6785baa387febe37681cb67
marjunstudio <uebujie@gmail.com>
2023-02-22 21:31:09 +0900

    Add:不要なファイルの削除、アクションの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,4 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path, warning: t('defaults.warning')
   end
-
-  def set_category
-    @category = Category.ransack(params[:q])
-    @categories = @category.result
-  end
-
-  def set_composer
-    @composer = Composer.ransack(params[:q])
-    @composers = @composer.result
-  end
-
-  def set_music
-    @music = Music.ransack(params[:q])
-    @musics = @music.result.includes(:composer)
-  end
 end
